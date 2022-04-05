38c4f317323307c1a5ac772630abd06c5072b53c
Sri Pranav Srivatsavai <92122776+sripranav9@users.noreply.github.com>
2022-04-05 11:23:52 +0400

    Update digitalAnalog.ino

--- april5/digitalAnalog.ino
+++ april5/digitalAnalog.ino
@@ -28,21 +28,11 @@ void setup()
 
 void loop()
 {
-
-  // FADE ****
-   // set the brightness of pin 9:
-  digitalWrite(led, brightness);
-
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
-  // ****
+  //Switch - LED turns ON and OFF at a regular interval 
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
   
   //read the position of the pot
   potPosition = analogRead(A0);    //set potPosition to a number between 0 and 1023 based on how far the knob is turned
@@ -51,45 +41,40 @@ void loop()
   //this if-else loop changes which LED to blink based on the pot value
   // we specifically chose maxPotVal/2 to make it change for exactly half of pot value
   if (potPosition > maxPotPos/2) {
-    digitalWrite(redLEDPin, HIGH);           // Turn on the red LED
-    delay(maxPotPos/2);              // delay for around 500 milliseconds
-  
-    digitalWrite(redLEDPin, LOW);            // Turn off the red LED
-    delay(maxPotPos/2);              // delay for around 500 milliseconds
+    //if its more than the half of the half Pot value already taken, blink faster
+    if (potPosition > ( maxPotPos - maxPotPos/4)){
+      digitalWrite(redLEDPin, HIGH);           // Turn on the red LED
+      delay(maxPotPos/5);              // delay for around 500 milliseconds
 
-    digitalWrite(redLEDPin, HIGH);           // Turn on the red LED
-    delay(maxPotPos/5);              // delay for around 200 milliseconds
-  
-    digitalWrite(redLEDPin, LOW);            // Turn off the red LED
-    delay(maxPotPos/5);              // delay for around 200 milliseconds
-
+      digitalWrite(redLEDPin, LOW);            // Turn off the red LED
+      delay(maxPotPos/5);              // delay for around 500 milliseconds
+    }
+    //if its on the lwoer half of the chosen half, blink slower
+    else {
     digitalWrite(redLEDPin, HIGH);           // Turn on the red LED
     delay(maxPotPos/2);              // delay for around 500 milliseconds
   
     digitalWrite(redLEDPin, LOW);            // Turn off the red LED
     delay(maxPotPos/2);              // delay for around 500 milliseconds
+    }
     
   }
   else {
-    digitalWrite(blueLEDPin, HIGH);           // Turn on the blue LED
-    delay(maxPotPos/2);              // delay for around 500 milliseconds
-  
-    digitalWrite(blueLEDPin, LOW);            // Turn off the blue LED
-    delay(maxPotPos/2);              // delay for around 500 milliseconds
+    //if its less than the half of the half Pot value already taken, blink slower
+    if (potPosition < (maxPotPos/4)){
+      digitalWrite(blueLEDPin, HIGH);           // Turn on the red LED
+      delay(maxPotPos/2);              // delay for around 500 milliseconds
 
-    digitalWrite(blueLEDPin, HIGH);           // Turn on the blue LED
-    delay(maxPotPos/5);              // delay for around 200 milliseconds
+      digitalWrite(blueLEDPin, LOW);            // Turn off the red LED
+      delay(maxPotPos/2);              // delay for around 500 milliseconds
+    }
+    //if its on the upper half of the chosen half, blink faster
+    else {
+    digitalWrite(blueLEDPin, HIGH);           // Turn on the red LED
+    delay(maxPotPos/5);              // delay for around 500 milliseconds
   
-    digitalWrite(blueLEDPin, LOW);            // Turn off the blue LED
-    delay(maxPotPos/5);              // delay for around 200 milliseconds
-
-    digitalWrite(blueLEDPin, HIGH);           // Turn on the blue LED
-    delay(maxPotPos/2);              // delay for around 500 milliseconds
-  
-    digitalWrite(blueLEDPin, LOW);            // Turn off the LED
-    delay(maxPotPos/2);              // delay for around 500 milliseconds
+    digitalWrite(blueLEDPin, LOW);            // Turn off the red LED
+    delay(maxPotPos/5);              // delay for around 500 milliseconds
+    }
   }
-
-
-
 }
